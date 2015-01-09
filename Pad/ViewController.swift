68839eb86756d033fd69f9373edd461d78d68b58
skyline75489 <skyline75489@outlook.com>
2015-01-09 21:24:50 +0800

    first commit

--- Pad/ViewController.swift
+++ Pad/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import Cocoa
+import WebKit
+import Markingbird
 
 class ViewController: NSViewController {
 
+    @IBOutlet var textView: NSTextView!
+    @IBOutlet weak var scrollView: NSScrollView!
+    @IBOutlet weak var webView: WebView!
+    
+    var markdown: Markdown?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var options = MarkdownOptions()
+        options.autoHyperlink = true
+        options.autoNewlines = true
+        options.emptyElementSuffix = ">"
+        options.encodeProblemUrlCharacters = true
+        options.linkEmails = false
+        options.strictBoldItalic = true
+        self.markdown = Markdown(options: options)
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: Selector("textDidChange"), name: NSTextDidChangeNotification, object:textView)
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +39,12 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    func textDidChange() {
+        if let text = textView.textStorage?.string {
+            let html = markdown?.transform(text)
+            self.webView.mainFrame.loadHTMLString(html, baseURL: nil)
+        }
+    }
 }
 
