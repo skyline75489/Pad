ee7357ac12fdc8152e2cd159bea79b468d978ca0
skyline75489 <skyline75489@outlook.com>
2015-01-09 21:04:49 +0800

    Initial Commit

---
+++ Pad/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pad
+//
+//  Created by skyline on 15/1/9.
+//  Copyright (c) 2015年 skyline. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pad/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pad
+//
+//  Created by skyline on 15/1/9.
+//  Copyright (c) 2015年 skyline. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
